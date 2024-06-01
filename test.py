8f693cef65e4f571e2b26a164546979ed7bd8e7c
GrFeg <grfeg@outlook.fr>
2024-06-01 16:03:17 +0200

    test pour commit

--- test.py
+++ test.py
@@ -6,5 +6,11 @@ date_du_jour = datetime.now()
 print(date_du_jour.day)
 
 
+def trouver_jour(date):
+    liste_jours = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']
+    date = date.lower().split()
 
+    if date[0] in liste_jours:
+        print('oui !')
 
+trouver_jour('lundi 12 juin')
